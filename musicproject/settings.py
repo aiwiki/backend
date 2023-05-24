1f9e715f8acc80b1ab12445ce8932e481c6c03b6
goiliace <zombizombi51@gmail.com>
2023-05-25 04:12:11 +0700

    ok

--- musicproject/settings.py
+++ musicproject/settings.py
@@ -150,9 +150,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/build/static')
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://192.168.56.1:3000"
